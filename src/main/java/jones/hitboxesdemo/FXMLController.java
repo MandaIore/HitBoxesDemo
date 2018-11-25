1f28b4176a1f7cb15044d6a510c1cbe01409273b
MandaIore <44068093+MandaIore@users.noreply.github.com>
2018-11-25 14:35:47 -0500

    added comments

--- src/main/java/jones/hitboxesdemo/FXMLController.java
+++ src/main/java/jones/hitboxesdemo/FXMLController.java
@@ -2,67 +2,54 @@ package jones.hitboxesdemo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.ThreadLocalRandom;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Bounds;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.util.Duration;
+
+/* Imoprts for rectangle
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Rectangle;
+*/
 
 public class FXMLController implements Initializable {
 
     @FXML
     private Polygon polRock;
     @FXML
-    private Rectangle recCar;
+    private Polygon polCar;
     @FXML
-private Pane panRock;
+    private Pane panRock;
     @FXML
-private Pane panCar;
-     Timeline movement = new Timeline(new KeyFrame(Duration.millis(8), ae -> move()));
-     
-   
-    
-     private void move() {
-     System.out.println("working");
-         if (panRock.getTranslateX() != 0){
-              System.out.println("moving");
-          panRock.setTranslateX(panRock.getTranslateX()-5);
-         }
-         else{
-             panRock.setTranslateX(900);
-         }
-         
-         if(checkCol(recCar, polRock) == true){
-              System.out.println("hit");
-               panRock.setTranslateX(900);
-                 }
-         
-     }
-     
-       private boolean checkCol(Shape obj1, Shape obj2) {
+    private Pane panCar;
+    Timeline movement = new Timeline(new KeyFrame(Duration.millis(8), ae -> move()));
+
+    private void move() {
+
+        if (panRock.getTranslateX() != 0) {
+
+            panRock.setTranslateX(panRock.getTranslateX() - 5);
+        } else {
+            panRock.setTranslateX(900);
+        }
+
+        if (checkCol(polCar, polRock) == true) {
+
+            panRock.setTranslateX(1000);
+        }
+
+    }
+
+    private boolean checkCol(Shape obj1, Shape obj2) {
         Shape intersect = Shape.intersect(obj1, obj2);
 
         return intersect.getBoundsInParent().getWidth() > 0;
 
-    }
-     
-
-    private Rectangle createBoundsRectangle(Bounds bounds) {  //method used to make the blank copy in other pane
-        Rectangle rect = new Rectangle();
-        rect.setFill(Color.TRANSPARENT);
-        rect.setX(bounds.getMinX());
-        rect.setY(bounds.getMinY());
-        rect.setWidth(bounds.getWidth());
-        rect.setHeight(bounds.getHeight());
-        return rect;
     }
 
     private Polygon createBoundsPolygon() {  //method used to make the blank copy in other pane
@@ -75,8 +62,17 @@ private Pane panCar;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         panCar.getChildren().add(createBoundsPolygon());
-          movement.setCycleCount(Timeline.INDEFINITE);
-         movement.play();
+        panCar.getChildren().add(createBoundsPolygon());
+        movement.setCycleCount(Timeline.INDEFINITE);
+        movement.play();
     }
 }
+/*private Rectangle createBoundsRectangle(Bounds bounds) {  //method used to make the blank copy in other pane
+Rectangle rect = new Rectangle();
+rect.setFill(Color.TRANSPARENT);
+rect.setX(bounds.getMinX());
+rect.setY(bounds.getMinY());
+rect.setWidth(bounds.getWidth());
+rect.setHeight(bounds.getHeight());
+return rect;
+}*/
